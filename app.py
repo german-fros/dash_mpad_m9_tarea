644246e4aa225f36ae57f1401ce79086b187e7b8
German Fros <german.fros551@gmail.com>
2025-06-27 20:37:05 -0300

    feat(login): implementar funcionalidad logout con redirección

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dash import Dash, dcc, html, Input, Output, State
 import dash_bootstrap_components as dbc
 from flask import Flask
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 from login_manager import get_user
 
@@ -44,6 +44,9 @@ def display_page(pathname):
     else:
         return html.Div([
             html.H1("Página principal"),
+            html.P("Bienvenido, estás logueado"),
+            dbc.Button("Cerrar Sesión", id="logout-button", color="secondary"),
+            html.Div(id="logout-output"),
             dcc.Link("Ir a Login", href="/login")
         ])
 
@@ -62,5 +65,16 @@ def login_callback(n_clicks, username, password):
         return dcc.Location(href="/", id="redirect")
     return dbc.Alert("Credenciales incorrectas", color="danger")
 
+# Callback del logout
+@app.callback(
+    Output("logout-output", "children"),
+    Input("logout-button", "n_clicks"),
+    prevent_initial_call=True
+)
+def logout_callback(n_clicks):
+    logout_user()
+    return dcc.Location(href="/login", id="redirect-logout")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
